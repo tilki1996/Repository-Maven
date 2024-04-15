85e3b358a68af52638f920ec39a2efbcb97f52fa
tilki1996 <emre.tilki@hs-weingarten.de>
2024-04-15 14:40:32 +0200

    Initialer Commit

---
+++ de.tr.me.seinTest/src/main/java/de/tr/me/de/tr/me/seinTest/App.java
@@ -0,0 +1,22 @@
+package de.tr.me.de.tr.me.seinTest;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hallo ich übernehme abnun die Arbeit!" );
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://selenium.dev");
+
+      
+    }
+}
